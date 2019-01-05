2cbf314c7e8a28e1e817e7a416114db57bb571b0
MugnierJu <mugnier.julien@hotmail.fr>
2019-01-05 19:12:54 +0100

    CRON

--- Backend/backend/src/main/java/optimaculture/backend/App.java
+++ Backend/backend/src/main/java/optimaculture/backend/App.java
@@ -1,11 +1,16 @@
 package optimaculture.backend;
 
+import java.util.Timer;
+
+import optimaculture.backend.services.ExportService;
 import optimaculture.backend.services.MqttImportService;
 
 public class App 
 {
     public static void main( String[] args )
     {
+        Timer timer = new Timer();
+    	timer.scheduleAtFixedRate(ExportService.getInstance(), 0, 10000);
     	MqttImportService.getInstance();
     }
 }
---
+++ Backend/backend/src/main/java/optimaculture/backend/services/ExportService.java
@@ -0,0 +1,63 @@
+package optimaculture.backend.services;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimerTask;
+
+import optimaculture.backend.utils.PropertiesService;
+
+public class ExportService extends TimerTask {
+	
+	private static ExportService instance = null;
+	
+	public ExportService() {
+		
+	}
+	
+	public static ExportService getInstance() {
+		if(instance == null) {
+			instance = new ExportService();
+		}
+		return instance;
+	}
+
+	@Override
+	public void run() {
+		List<File> fileList = getFiles();
+		if(fileList.size() > 0) {
+		pushToElastic(fileList);
+			//TODO activer ça quand le code de push marche
+			//removeFiles(fileList);
+		}
+	}
+	
+	/**
+	 * 
+	 * @return la liste des fichiers à push
+	 */
+	private List<File> getFiles() {
+		File tempFiledirectory = new File(System.getProperty("java.io.tmpdir"));
+		List<File> fileList = new ArrayList<File>();
+		
+		for (final File fileEntry : tempFiledirectory.listFiles()) {
+	        if (!fileEntry.isDirectory() && fileEntry.getName().startsWith(PropertiesService.getInstance().getProperty("outputFileName"))) {
+	            System.out.println(fileEntry.getName());
+	            fileList.add(fileEntry);
+	        }
+	    }
+		return null;
+	}	
+	
+	private void pushToElastic(List<File> fileList) {
+		//TODO faire le code curl ici
+	}
+	
+	private void removeFiles(List<File> fileList) {
+		for (final File fileEntry : fileList) {
+			try {
+				fileEntry.delete();
+			}catch(Exception e) {}
+	    }
+	}
+}
